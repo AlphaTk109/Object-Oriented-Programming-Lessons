3d272fb680aed5c510729e7268ea891217de26b9
子健 刘 <alphatk@vip.qq.com>
2020-08-08 19:39:36 +0800

    运算符重载2020-08-08

--- 01-Basics基础整合/02-运算符重载基础-Overloading Basics.cpp
+++ 01-Basics基础整合/02-运算符重载基础-Overloading Basics.cpp
@@ -35,6 +35,7 @@ int main()
 	return 0;
 }
 */
+
 //02-改写01：对运算符 + 进行重载
 /*
 class Complex
@@ -79,6 +80,7 @@ int main()
 	cout << "c4=c1+c2=";c4.display();
 	return 0;
 }*/
+
 //03-重载双目运算符（书309）
 /*
 class String
@@ -112,5 +114,89 @@ int main()
 {
 	String string1("Hello"), string2("Book");
 	cout << (string1 > string2) << endl;
+}
+*/
+
+//04-重载流插入“<<”与输出“>>”实例
+/*
+//ostream-output-<<输出实例
+class Complex
+{
+public:
+	Complex() { real = 0;imag = 0; }
+	Complex(double r, double i) { real = r;imag = i; }
+	Complex operator+(Complex& c2);
+	friend ostream& operator << (ostream&, Complex&); //运算符"<<"重载为友元函数
+private:
+	double real;
+	double imag;
+};
+Complex Complex::operator+(Complex& c2)
+{
+	return Complex(real + c2.real, imag + c2.imag);
+}
+ostream& operator<<(ostream& output, Complex& c)
+{
+	output<<"(" << c.real << "+" << c.imag << "i)" << endl;
+	return output;
+}
+int main()
+{
+	Complex c1(2, 4), c2(6, 10), c3;
+	c3 = c1 + c2;
+	cout << c3;
+	return 0;
+}
+
+//istream-input->>输入实例
+class Complex
+{
+public:
+	friend ostream& operator<<(ostream&, Complex&);
+	friend istream& operator>>(istream&, Complex&);
+private:
+	double real;
+	double imag;
+};
+ostream& operator<<(ostream& output, Complex& c)
+{
+	output << "(" << c.real << "+" << c.imag << "i)";
+	return output;
+}
+istream& operator>>(istream& input, Complex& c)
+{
+	cout << "input real part and imaginary pary of complex number:";
+	input >> c.real >> c.imag;
+	return input;
+}
+int main()
+{
+	Complex c1, c2;
+	cin >> c1 >> c2;
+	cout << "c1=" << c1 << endl;
+	cout << "c2=" << c2 << endl;
+	return 0;
+}
+*/
+
+//05-将一个double数据与Complex类数据相加(强制类型转换)
+/*
+class Complex
+{
+public:
+	Complex() { real = 0;imag = 0; }
+	Complex(double r, double i) { real = r;imag = i; }
+	operator double() { return real; }		//定义类型转换函数
+private:
+	double real;
+	double imag;
+};
+int main()
+{
+	Complex c1(3, 4), c2(5, -10), c3;
+	double d;
+	d = 2.5 + c1;
+	cout << d << endl;
+	return 0;
 }
 */
